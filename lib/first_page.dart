a265792e15d0bd9130e02cf3948fd1b136cb7e53
rbourque7 <59621985+rbourque7@users.noreply.github.com>
2020-10-15 15:36:18 -0600

    commit1

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -8,6 +8,7 @@ class MyFirstPage extends StatefulWidget {
 class _MyFirstPageState extends State<MyFirstPage> {
   bool _enabled = false;
   String _msg1 = '';
+  String _msg2 = '';
 
   @override
   Widget build(BuildContext context) {
@@ -29,18 +30,18 @@ class _MyFirstPageState extends State<MyFirstPage> {
       }
     }
 
-    Object onPressed2() {
-      if (_enabled) {
-        print(
-            'onPressed2 returning the result of running the anonymous function');
-        return () {
-          print('Anon func now running');
-        }();
-      } else {
-        print('onPressed2 returning NULL');
-        return null;
-      }
-    }
+    //Object onPressed2() {
+      //if (_enabled) {
+        //print(
+            //'onPressed2 returning the result of running the anonymous function');
+        //return () {
+         // print('Anon func now running');
+        //}();
+     // } else {
+        //print('onPressed2 returning NULL');
+       // return null;
+      //}
+   // }
 
     print('The build is being RUN');
 
@@ -62,11 +63,15 @@ class _MyFirstPageState extends State<MyFirstPage> {
                       _enabled = onChangedValue;
                       setState(() {
                         if (_enabled) {
-                          _msg1 = 'Enabled';
-                          print('_enabled is true');
+                          _msg1 = 'Click Me';
+                          print('_Click Me is true');
+                          _msg2 = 'Reset';
+                          print('_Reset is true');
                         } else {
                           _msg1 = '';
                           print('_enabled is false');
+                          _msg2 = '';
+                          print('_Reset is false');
                         }
                       });
                     }),
@@ -88,6 +93,21 @@ class _MyFirstPageState extends State<MyFirstPage> {
                     padding: EdgeInsets.all(20.0),
                     onPressed: onPressed1(),
                     child: Text(_msg1),
+                  ),
+                ),
+                 Container(
+                  margin: EdgeInsets.all(20),
+                  child: MaterialButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    elevation: 8,
+                    textColor: Colors.blue,
+                    color: Colors.redAccent,
+                    highlightColor: Colors.blue,
+                    splashColor: Colors.white,
+                    padding: EdgeInsets.all(20.0),
+                    onPressed: onPressed1(),
+                    child: Text(_msg2),
                   ),
                 ),
               ],
